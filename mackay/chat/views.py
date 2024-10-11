3a69c1ad739268eb5608e2009439d07c38d55280
tony751015 <clouds0916@gmail.com>
2024-10-11 15:47:50 +0800

    10/11 update

--- mackay/chat/views.py
+++ mackay/chat/views.py
@@ -57,55 +57,94 @@ def chat_record_list(request):
       page = serializer['page']
       size = serializer['size']
 
+      ISOTIMEFORMAT = '%Y-%m-%d'
+
+      recordData = []
+
       # 取得某會員 指定Field的對話記錄 
       recordQuery = chat_record.objects.filter(Q(create_user=userId))
       recordCount = recordQuery.count() # 找到的資料數量
-      recordData = recordQuery.order_by('-create_date').annotate(record_id=F('id')).values('record_id', 'content', 'content_type', 'create_date', 'is_carer_user') # 取出指定Field
+      # recordData = recordQuery.order_by('-create_date').annotate(record_id=F('id')).values('record_id', 'content', 'content_type', 'create_date', 'is_carer_user') # 取出指定Field
+      # recordData = recordQuery.order_by('-create_date').annotate(record_id=F('id')).values_list('create_date')
+      # recordData = recordQuery.order_by('-create_date').annotate(record_id=F('id'))
+
+      # [
+      #   {
+      #     "group": [{}, {}]
+      #   },
+      #   {
+      #     "group": [{}, {}]
+      #   },
+      #   {
+      #     "group": [{}, {}]
+      #   }
+      # ]
+
+      # for splitGroup in gg:
+      #   splitGroupData = splitGroup['group']
+
+      #   for item, index in splitGroupData:
+      #     dateValue = item['create_data']
+      #     item['isFirstData'] = ''
+          
+      #     if index == 0:
+      #       item['isFirstData'] = dateValue.strftime(ISOTIMEFORMAT)
+
+      #     recordData.append({
+      #       "isFirstData": item['isFirstData'],
+      #       # //..
+      #     })
+
+
+      print('test split group: ', recordData)
+      return Response(recordData, status=200)
+
+
     
-      # For迴圈
-      for items in recordData:
-        eachUserId = User.objects.get(id=userId) # 取得每筆資料的User檔案，因為id是唯一，所以用GET
-        getUserName = eachUserId.name # 會員名稱
-        getUserGender = eachUserId.gender # 會員性別
-        oldDate = items['create_date'] # 先記錄原本日期
+      # # For迴圈
+      # for items in recordData:
+      #   eachUserId = User.objects.get(id=userId) # 取得每筆資料的User檔案，因為id是唯一，所以用GET
+      #   getUserName = eachUserId.name # 會員名稱
+      #   getUserGender = eachUserId.gender # 會員性別
+      #   oldDate = items['create_date'] # 先記錄原本日期
 
-        # 轉換格式
-        newDateTime = oldDate.strftime('%Y-%m-%d') # 轉換格式 YYYY-MM-DD
-        # 每筆資料額外添加 user_name / gender的資料
-        items['user_name'] = getUserName
-        items['gender'] = getUserGender
-        # 替換原本create_date的資料內容
-        items['create_date'] = newDateTime
+      #   # 轉換格式
+      #   newDateTime = oldDate.strftime('%Y-%m-%d') # 轉換格式 YYYY-MM-DD
+      #   # 每筆資料額外添加 user_name / gender的資料
+      #   items['user_name'] = getUserName
+      #   items['gender'] = getUserGender
+      #   # 替換原本create_date的資料內容
+      #   items['create_date'] = newDateTime
 
-      try:
-        p = Paginator(recordData, size) 
-        page1 = p.page(page)
-        final = page1.object_list 
-        results = {
-          "count": recordCount,
-          "results": final
-        }
-        return Response(results, status=200)
+      # try:
+      #   p = Paginator(recordData, size) 
+      #   page1 = p.page(page)
+      #   final = page1.object_list 
+      #   results = {
+      #     "count": recordCount,
+      #     "results": final
+      #   }
+      #   return Response(results, status=200)
       
-      # 如果超出分頁範圍
-      except PageNotAnInteger:
-        results = {
-          "count": recordCount,
-          "results": []
-        }
-        return Response(results, status=200)
+      # # 如果超出分頁範圍
+      # except PageNotAnInteger:
+      #   results = {
+      #     "count": recordCount,
+      #     "results": []
+      #   }
+      #   return Response(results, status=200)
 
-      # 如果指定分頁沒有資料
-      except EmptyPage:
-        results = {
-          "count": recordCount,
-          "results": []
-        }
-        return Response(results, status=200)
+      # # 如果指定分頁沒有資料
+      # except EmptyPage:
+      #   results = {
+      #     "count": recordCount,
+      #     "results": []
+      #   }
+      #   return Response(results, status=200)
       
-      # 發生其他錯誤時
-      except:
-        return Response('error', status=500)
+      # # 發生其他錯誤時
+      # except:
+      #   return Response('error', status=500)
       
     else:
       return Response('need params', status=500)
